55a31d6e7760049ea9856b0af6a5e7b4debbc42d
adonocolor <macccich@gmail.com>
2023-10-01 16:04:03 +0300

    feat: @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { BookModule } from './book/book.module';
 import { DatabaseModule } from './database/database.module';
 import { SubscriptionModule } from './subscription/subscription.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, BookModule, DatabaseModule, SubscriptionModule],
+  imports: [UserModule, BookModule, DatabaseModule, SubscriptionModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -3,16 +3,17 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import { User } from "../user/entities/user.entity";
 import { Book } from "../book/entities/book.entity";
 import { Subscription } from "../subscription/entities/subscription.entity";
+import * as process from "process";
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'ado',
-      database: 'books',
+      type: "postgres",
+      host: process.env.PGHOST,
+      port: parseInt(process.env.PGPORT),
+      username: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
       entities: [User, Book, Subscription],
       synchronize: false,
       autoLoadEntities: true,
